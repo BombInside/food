a12cdd140ce7e248901ac0d3c7a63ac40cff5801
Prelang Builder <builder@prelang.com>
2016-07-19 08:36:31 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/restoraunts_controller.rb
+++ app/controllers/restoraunts_controller.rb
@@ -1,4 +1,8 @@
 class RestorauntsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_restoraunt, only: [:show, :edit, :update, :destroy]
 
   # GET /restoraunts
